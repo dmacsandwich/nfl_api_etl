ebb73cadab86d58d14ad7482df8640a6fc9d7a84
dmacsandwich <120608633+dmacsandwich@users.noreply.github.com>
2023-09-28 23:45:49 -0400

    upated main file to follow functional format

--- football_team_id.py
+++ football_team_id.py
@@ -3,8 +3,9 @@ from api.nfl_api import NflApi
 URL = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams'
 data = NflApi().get_data_from_url(url=URL)
 
-num_of_teams=len(data['sports'][0]['leagues'][0]['teams'])
-for i in range(num_of_teams):
-    print(data['sports'][0]['leagues'][0]['teams'][i]['team']['id'] \
-    ,data['sports'][0]['leagues'][0]['teams'][i]['team']['displayName'])
+def get_team_ids() ->list:
+    num_of_teams=len(data['sports'][0]['leagues'][0]['teams'])
+    return [data['sports'][0]['leagues'][0]['teams'][i]['team']['id'] \
+        ,data['sports'][0]['leagues'][0]['teams'][i]['team']['displayName'] \
+            for i in range(num_of_teams)]
 #print(data['sports'][0]['leagues'][0]['teams'][0]['team'])
