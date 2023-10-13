ff92b7f82e470713e2d62188d67bfbe48adbcda2
dmacsandwich <120608633+dmacsandwich@users.noreply.github.com>
2023-10-12 21:00:49 -0400

    Add test files

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
 TEAMS_IDS = f'https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams'
 
 #TODO add a dynamic way to update this url
-GAME_IDS = f'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?limit=1000&dates=2021&seasontype=2'
+GAME_IDS = f'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?limit=1000&dates={YEAR}&seasontype=2'
