b0e4869a562a5d40a93dd9595eb0128c291b79f8
jinsoowhang <jwtreones@gmail.com>
2023-11-07 08:51:26 -0800

    move nfl_df.py from etl\data\raw

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -3,6 +3,7 @@ import pandas as pd
 team_df = pd.read_parquet("etl/data/raw/team_ids.parquet")
 match_df = pd.read_parquet("etl/data/raw/match_ids.parquet")
 athlete_df = pd.read_parquet("etl/data/raw/athlete_ids.parquet")
+nfl_df = pd.read_parquet("etl/data/raw/nfl_df.parquet")
 
-print(len(athlete_df))
-print(athlete_df.columns)
+print(len(nfl_df))
+print(nfl_df.head())
